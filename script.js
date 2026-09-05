d27a05d7da8897ba9b05cb5129e2f553b6031e03
becausecannotbeblank <resubuhtig@proton.me>
2026-09-05 18:45:07 +0200

    Added add book to array functionality. Fixed bug that had form refreshing page after adding with preventDefault.

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const myLibrary = [];
 
-function Book(title, author, genre, pages, read, notes, score){
+const btn = document.querySelector("button");
+
+function Book(title, author, genre, pages, read, notes){
     this.title = title;
     this.author = author;
     this.genre = genre;
     this.pages = pages;
     this.read = read;
     this.notes = notes;
-    this.score = score;
+ /*   this.score = score;*/
 }
 
-function addBook(){
-    const book = new Book("","","","","","","")
-    book.push("myLibrary");
+function addBook(e){
+    e.preventDefault();
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let genre = document.getElementById("genre").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+    let notes = document.getElementById("notes").value;
+  /*  let score = document.getElementById("score").value;*/
+    
+    const book = new Book(title, author, genre, pages, read, notes)
+    myLibrary.push(book);
+    console.log(myLibrary.length);
 }
+
+btn.addEventListener("click", addBook);
